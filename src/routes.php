39a8940729dbc389be055489362c012c622b092c
nunulk <nunulk@usingstd.com>
2018-02-27 17:36:17 +0900

    Implement create

--- src/routes.php
+++ src/routes.php
@@ -7,14 +7,27 @@ use Slim\Http\Response;
 
 // 一覧表示
 $app->get('/tickets', function (Request $request, Response $response) {
+    return $response->write('tickets');
 });
 
 // 新規作成用フォームの表示
 $app->get('/tickets/create', function (Request $request, Response $response) {
+    return $this->renderer->render($response, 'tasks/create.phtml');
 });
 
 // 新規作成
 $app->post('/tickets', function (Request $request, Response $response) {
+    $subject = $request->getParsedBodyParam('subject');
+    // ここに保存の処理を書く
+    $sql = 'INSERT INTO tickets (subject) values (:subject)';
+    $stmt = $this->db->prepare($sql);
+    $result = $stmt->execute(['subject' => $subject]);
+    if (!$result) {
+        throw new \Exception('could not save the ticket');
+    }
+
+    // 保存が正常にできたら一覧ページへリダイレクトする
+    return $response->withRedirect("/tickets");
 });
 
 // 表示
