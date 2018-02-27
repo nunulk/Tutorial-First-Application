edac7aacd075b7fc6aae34a218373597a38884ac
nunulk <nunulk@usingstd.com>
2018-02-27 13:39:59 +0900

    Add routes

--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,34 @@ use Slim\Http\Response;
 
 // Routes
 
+// 一覧表示
+$app->get('/tickets', function (Request $request, Response $response) {
+});
+
+// 新規作成用フォームの表示
+$app->get('/tickets/create', function (Request $request, Response $response) {
+});
+
+// 新規作成
+$app->post('/tickets', function (Request $request, Response $response) {
+});
+
+// 表示
+$app->get('/tickets/{id}', function (Request $request, Response $response, array $args) {
+});
+
+// 編集用フォームの表示
+$app->get('/tickets/{id}/edit', function (Request $request, Response $response, array $args) {
+});
+
+// 更新
+$app->put('/tickets/{id}', function (Request $request, Response $response, array $args) {
+});
+
+// 削除
+$app->delete('/tickets/{id}', function (Request $request, Response $response, array $args) {
+});
+
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
