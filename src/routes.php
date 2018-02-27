6323714daf468c89c61bbc90f7532875abceb05e
nunulk <nunulk@usingstd.com>
2018-02-27 22:17:59 +0900

    Add edit page

--- src/routes.php
+++ src/routes.php
@@ -55,6 +55,18 @@ $app->get('/tickets/{id}', function (Request $request, Response $response, array
 
 // 編集用フォームの表示
 $app->get('/tickets/{id}/edit', function (Request $request, Response $response, array $args) {
+    $sql = 'SELECT * FROM tickets WHERE id = :id';
+    $stmt = $this->db->prepare($sql);
+    $result = $stmt->execute(['id' => $args['id']]);
+    if (!$result) {
+        throw new \Exception('could not find the ticket');
+    }
+    $ticket = $stmt->fetch();
+    if (!$ticket) {
+        return $response->withStatus(404)->write('not found');
+    }
+    $data = ['ticket' => $ticket];
+    return $this->renderer->render($response, 'tasks/edit.phtml', $data);
 });
 
 // 更新
